78f2c55b70619fbb9ac5b7f9bd29d3897be5df21
jaypventuresllc <jaypventures@icloud.com>
2026-04-02 22:27:07 -0600

    yes

--- sandbox/index.js
+++ sandbox/index.js
@@ -1,2 +1,18 @@
+import wixEditor from "wix-editor";
+
 // Entry point for sandbox experiments
 console.log('Sandbox environment is ready.');
+
+wixEditor.getCurrentViewport().then((viewport) => {
+  // Your code that uses the 'viewport' value goes here
+  console.log('Current viewport:', viewport);
+  /* For example, `viewport` can be: 
+    {
+      type: "DESKTOP",
+      range: {
+        minWidth: 1001,
+        maxWidth: undefined
+      }
+    }
+  */
+});
