dbbd889b7c6fb59e87780148dfcb81779cf51734
jaypventuresllc <jayhere@jaypventuresllc.com>
2026-05-01 00:09:18 -0600

    Update Worker response to 'JPV Worker Live'

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 export default {
   async fetch(request, env, ctx) {
-    return new Response("Hello from JayPVentures API!", { status: 200 });
+    return new Response("JPV Worker Live", { status: 200 });
   }
 }
